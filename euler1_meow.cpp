3fc2d5723eaa938039c0f80f7eda88d8ade95005
Truong Le Gia Luat <sohk1997@gmail.com>
2018-08-31 22:06:18 +0700

    Add euler 1 meo meo version

---
+++ euler1_meow.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define meo while
+#define meoo cin
+#define meeo cout
+#define meomeo int
+#define meomeomeo 3
+#define meomeomeoo 5
+#define meoomeoomeoo 15
+#define meoooo 1
+#define mmeeoo 2
+#define mmmeoo +
+#define mmeeeo /
+#define meeooo -
+#define mmeooo *
+#define mew >>
+#define meww <<
+#define meewmeew --
+#define meeww long long
+#define meew =
+#define mmeoo %
+#define meeoow ==
+#define mmmeo (
+#define meooo )
+#define mewww 0
+#define mewmew endl
+#define mmeww +=
+
+using namespace std;
+
+int main()
+{
+    meomeo meomeoo;
+    meoo mew meomeoo;
+    meo mmmeo meomeoo meewmeew meooo
+    {
+        meomeo meoomeoo;
+        meoo mew meoomeoo;
+        meeww meeeo meew meoomeoo mmeeeo meomeomeo meeooo mmmeo meoomeoo mmeoo meomeomeo  meeoow mewww meooo;
+        meeww meeeeeo meew meoomeoo mmeeeo meomeomeoo meeooo mmmeo meoomeoo mmeoo meomeomeoo  meeoow mewww meooo;
+        meeww meeeeeeeo meew meoomeoo mmeeeo meoomeoomeoo meeooo mmmeo meoomeoo mmeoo meoomeoomeoo  meeoow mewww meooo;
+        meeww mewmewmew meew mmmeo mmmeo meeeo mmeooo mmmeo meeeo mmmeoo meoooo meooo meooo mmeeeo mmeeoo meooo mmeooo meomeomeo;
+        mewmewmew mmeww mmmeo mmmeo meeeeeo mmeooo mmmeo meeeeeo mmmeoo meoooo meooo meooo mmeeeo mmeeoo meooo mmeooo meomeomeoo;
+        mewmewmew mmeww meeooo mmmeo mmmeo meeeeeeeo mmeooo mmmeo meeeeeeeo mmmeoo meoooo meooo meooo mmeeeo mmeeoo meooo mmeooo meoomeoomeoo;
+        meeo meww mewmewmew meww mewmew;
+    }
+}
